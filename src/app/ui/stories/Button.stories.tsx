33626ebd8a437a91550370f647e4fd87d24396b0
elVengador <jimynicanor1001@gmail.com>
2021-09-30 23:09:29 -0500

    litle advances

--- src/app/ui/stories/Button.stories.tsx
+++ src/app/ui/stories/Button.stories.tsx
@@ -4,34 +4,59 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Button } from './Button';
 
 export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: 'Design System/Atoms/Button',
+    component: Button,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    parameters: {
+        backgroundColor: {
+            values: [
+                { name: 'red', value: '#f00' },
+                { name: 'green', value: '#0f0' },
+                { name: 'blue', value: '#00f' },
+            ],
+        },
+    },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
+    primary: true,
+    label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+    label: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+    size: 'large',
+    label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+    size: 'small',
+    label: 'Button',
+};
+
+export const Tmp = Template.bind({})
+Tmp.args = {
+    size: 'small',
+    label: 'Tmp',
+    backgroundColor: 'red'
+}
+Tmp.parameters = {
+    backgroundColor: {
+        values: [
+            { name: 'red', value: '#f00' },
+            { name: 'green', value: '#0f0' },
+            { name: 'blue', value: '#00f' },
+        ],
+    },
 };
