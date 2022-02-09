ea934413e5feb7346486faccf94fd901fea13aa7
elVengador <jimynicanor1001@gmail.com>
2022-02-08 21:17:36 -0500

    update path in server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
