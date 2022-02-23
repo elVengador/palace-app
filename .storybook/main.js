a0c92bddbe18fada3a2ae0095121c38f2db0645c
elVengador <jimynicanor1001@gmail.com>
2022-02-22 20:51:21 -0500

    configuration to alert component with a11y and interaction test

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,9 @@ module.exports = {
     "addons": [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
-        "@storybook/preset-scss"
+        "@storybook/preset-scss",
+        '@storybook/addon-a11y', //👈 The a11y addon goes here
+        '@storybook/addon-interactions', //👈 The interactions addon goes here
     ],
     "core": {
         "builder": "webpack5"
