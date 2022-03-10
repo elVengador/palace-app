2669c4746c57d6df3333e94b8d708bec0357cb98
elVengador <jimynicanor1001@gmail.com>
2022-03-10 16:25:26 -0500

    update config in webpack to pwa

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -3,23 +3,9 @@ import { Configuration } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import ESLintPlugin from "eslint-webpack-plugin";
+import CopyWebpackPlugin from 'copy-webpack-plugin'
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 import webpack from "webpack";
-
-// const getEnvKeys = () => {
-//     const env = dotenv.config().parsed;
-//     let envKeys = {}
-//     if (env) {
-//         envKeys = Object.keys(env).reduce((acu, cur) => {
-//             const value = JSON.stringify(env[cur]);
-//             // acu[`process.env.${cur}`]
-//             const tt = `process.env.${cur}`
-//             return { ...acu, [tt]: value };
-//         }, {});
-//     }
-//     console.log('......???>>>', envKeys, process.env.API_HOST);
-//     return envKeys
-// }
 
 const getEnvKeys = () => {
 
@@ -27,7 +13,6 @@ const getEnvKeys = () => {
         'process.env.API_HOST': JSON.stringify(process.env.API_HOST),
         'process.env.API_PORT': JSON.stringify(process.env.API_PORT)
     }
-    console.log('......???>>>', envKeys);
     return envKeys
 }
 
@@ -65,11 +50,21 @@ const config: Configuration = {
         extensions: [".tsx", ".ts", ".js"],
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: "src/index.html" }),
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            favicon: path.resolve(__dirname, "src", "assets", "favicon.ico")
+        }),
         new ForkTsCheckerWebpackPlugin({ async: false }),
         new ESLintPlugin({ extensions: ["js", "jsx", "ts", "tsx"] }),
         new CleanWebpackPlugin(),
-        new webpack.DefinePlugin(getEnvKeys())
+        new webpack.DefinePlugin(getEnvKeys()),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets' },
+                { from: 'pwa.json', to: 'pwa.json' },
+                { from: 'sw.js', to: 'sw.js' }
+            ]
+        }),
     ],
     optimization: {
         splitChunks: {
